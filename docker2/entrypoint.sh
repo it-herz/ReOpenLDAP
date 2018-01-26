cc6e111ce9ed9c3128c1295fb9cc22ca16c20d08
Dmitrii Zolotov <dzolotov@herzen.spb.ru>
2018-01-26 15:50:00 +0300

    Add monitoring, replicator account, sssvlv config

--- docker2/entrypoint.sh
+++ docker2/entrypoint.sh
@@ -1,93 +1,98 @@
 #!/bin/bash
 
-ulimit -n 1024
+# Define environment
+ulimit -n 2048
 mkdir -p /home/ldap
 chown -R ldap:ldap /home/ldap
-
-mkdir -p /cache
-chown -R ldap:ldap /cache
 
 CONFIG_BASEDIR=/opt/reopenldap/etc/
 CONFIG_FILE=$CONFIG_BASEDIR/slapd.conf
 CONFIG_DIR=$CONFIG_BASEDIR/slapd.d
 DATA_DIR=/opt/reopenldap/var/reopenldap-data
+ACCESSLOG_DIR=/opt/reopenldap/var/accesslog
 
+# Fix permissions
+mkdir -p $CONFIG_DIR
 chown ldap:ldap -R $CONFIG_DIR
 chmod 700 -R $CONFIG_DIR
 
+mkdir -p $DATA_DIR
 chown ldap:ldap -R $DATA_DIR
 chmod 700 -R $DATA_DIR
 
-ACCESSLOG_DIR=/opt/reopenldap/var/accesslog
+export ENC_CONFIG_PASSWORD="$(slappasswd -h {ssha} -s $CONFIG_PASSWORD)"
+export ENC_ROOT_PASSWORD="$(slappasswd -h {ssha} -s $ROOT_PASSWORD)"
+export ENC_REPLICATOR_PASSWORD="$(slappasswd -h {ssha} -s $REPLICATOR_PASSWORD)"
 
-# Setup config directory
-
-if [ ! -d $ACCESSLOG_DIR ]
-then
-  mkdir -p $ACCESSLOG_DIR
-fi
-chown -R ldap:ldap $ACCESSLOG_DIR
-
+# First boot - raw initialization
 if [ ! -f $CONFIG_DIR/cn=config.ldif ]
 then
-  mkdir -p $CONFIG_DIR
-  chown ldap:ldap -R $CONFIG_DIR
-  chmod 700 -R $CONFIG_DIR
-
-  mkdir -p $DATA_DIR
-  chown ldap:ldap -R $DATA_DIR
-  chmod 700 -R $DATA_DIR
-
 # Setup config database
   echo "database config" >/tmp/slapd.conf
   echo 'rootdn "cn=admin,cn=config"' >>/tmp/slapd.conf
-  echo "rootpw $CONFIG_PASSWORD" >>/tmp/slapd.conf
+  echo "rootpw $ENC_CONFIG_PASSWORD" >>/tmp/slapd.conf
   echo "access to * by dn.exact=gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth manage by * break" >>/tmp/slapd.conf
-
+  echo "crash-backtrace on" >>/tmp/slapd.conf
   runuser -l ldap -c "/opt/reopenldap/sbin/slaptest -f /tmp/slapd.conf -F $CONFIG_DIR"
 
 # Setup domain database
   sed -i "s~^suffix.*$~suffix \"$LDAP_SUFFIX\"~g" $CONFIG_FILE
   sed -i "s~^rootdn.*$~rootdn \"$ROOT_DN\"~g" $CONFIG_FILE
+  sed -i "d/^index.*/" $CONFIG_FILE
 
-  sed -i "s~^rootpw.*$~rootpw $ROOT_PASSWORD~g" $CONFIG_FILE
-
+  sed -i "s~^rootpw.*$~rootpw $ENC_ROOT_PASSWORD~g" $CONFIG_FILE
   echo "access to * by dn.exact=gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth manage by * break" >>$CONFIG_FILE
 
-  cp /opt/schemas/* /opt/etc/reopenldap/schema/
-
-  echo "Initializing domain"
-  /opt/reopenldap/sbin/slapd -h "ldap://$HOSTNAME ldaps://$HOSTNAME ldapi:///" -u ldap -g ldap -d $LDAP_LOG_LEVEL -f $CONFIG_FILE &
+# Initializing domain
+  cp /opt/schemas/* /opt/reopenldap/etc/schema/
+  /opt/reopenldap/sbin/slapd -h "ldap://$HOSTNAME ldaps://$HOSTNAME ldapi:///" -u ldap -g ldap -d "$LDAP_LOG_LEVEL" -f "$CONFIG_FILE" &
   PID=$!
   sleep 3
   kill $PID
   sleep 1
 
+# Convert configuration to config backend
   runuser -l ldap -c "/opt/reopenldap/sbin/slaptest -f $CONFIG_FILE -F $CONFIG_DIR"
 
-  /opt/reopenldap/sbin/slapd -h "ldap://$HOSTNAME ldaps://$HOSTNAME ldapi:///" -u ldap -g ldap -d $LDAP_LOG_LEVEL -F /opt/reopenldap/etc/slapd.d &
+# Apply new configuration
+  /opt/reopenldap/sbin/slapd -h "ldap://$HOSTNAME ldaps://$HOSTNAME ldapi:///" -u ldap -g ldap -d "$LDAP_LOG_LEVEL" -F "$CONFIG_DIR" &
   PID=$!
   sleep 5
 
-  # add domain admin dn
   #register overlays
 
+  # Register cache overlay (if needed)
   if [ "$ENABLE_CACHE" == "1" ] && [ "$MODE" != "REPLICA" ]
   then
-     echo "olcModuleLoad: pcache" >>/opt/modules.ldif
+    mkdir -p /cache
+    chown -R ldap:ldap /cache
+    echo "olcModuleLoad: pcache" >>/opt/modules.ldif
   fi
 
+  # Register accesslog overlay (if needed)
   if [ "$ENABLE_ACCESSLOG" == "1" ] && [ "$MODE" != "REPLICA" ]
   then
+     # Setup accesslog directory
+
+     mkdir -p $ACCESSLOG_DIR
+     chown -R ldap:ldap $ACCESSLOG_DIR
      echo "olcModuleLoad: accesslog" >>/opt/modules.ldif
   fi
-
   
+  # Register modules
   ldapadd -H ldapi:/// -Y EXTERNAL -f /opt/modules.ldif
+
+
+  #ServerSideSort
+  ldapadd -H ldapi:/// -Y EXTERNAL -f /opt/sssvlv.ldif
+
+  #Audit log
+  ldapadd -H ldapi:/// -Y EXTERNAL -f /opt/audit.ldif
 
   #Syncrepl for data
   ldapadd -H ldapi:/// -Y EXTERNAL -f /opt/syncprov.ldif
 
+  # Build servers list and rid records
   declare -i rid
   rid=1
 
@@ -102,11 +107,11 @@ then
     rid=rid+1
   done
   echo "-" >>/tmp/schema_repl_db.ldif
+  # Turn on mirror mode
   cat /opt/mirror.ldif >>/tmp/schema_repl_db.ldif
   IFS="$OLD_IFS"
 
   ldapadd -H ldapi:/// -Y EXTERNAL -f /tmp/schema_repl_db.ldif
-  #rm /tmp/schema_repl_db.ldif
 
   #Syncrepl for config (schema and metadata)
   ldapadd -H ldapi:/// -Y EXTERNAL -f /opt/syncprovdb.ldif
@@ -126,30 +131,37 @@ then
   cat /opt/mirror.ldif >>/tmp/schema_repl.ldif
   ldapadd -H ldapi:/// -Y EXTERNAL -f /tmp/schema_repl.ldif
 
+
   if [ "$MODE" != "REPLICA" ]
-  then
+    then
 
-  #RefInt Configuration
-  ldapadd -H ldapi:/// -Y EXTERNAL -f /opt/refint.ldif
+    #RefInt Configuration
+    ldapadd -H ldapi:/// -Y EXTERNAL -f /opt/refint.ldif
 
-  #memberof overlay
-  ldapadd -H ldapi:/// -Y EXTERNAL -f /opt/memberof.ldif
+    #memberof overlay
+    ldapadd -H ldapi:/// -Y EXTERNAL -f /opt/memberof.ldif
 
-  #accesslog overlay
-  if [ "$ENABLE_ACCESSLOG" == "1" ]
-  then
-    cat /opt/accesslogdb.ldif | envsubst >/tmp/accesslogdb.ldif
-    ldapadd -H ldapi:/// -Y EXTERNAL -f /tmp/accesslogdb.ldif
+    if [ "$ENABLE_ACCESSLOG" == "1" ]
+    then
+      cat /opt/accesslogdb.ldif | envsubst >/tmp/accesslogdb.ldif
+      ldapadd -H ldapi:/// -Y EXTERNAL -f /tmp/accesslogdb.ldif
+      ldapadd -H ldapi:/// -Y EXTERNAL -f /opt/accesslog.ldif
 
-    ldapadd -H ldapi:/// -Y EXTERNAL -f /opt/accesslog.ldif
-  fi
+      cat /opt/accesslog_acl.ldif | envsubst >/tmp/accesslog_acl.ldif
+    fi
 
-  if [ "$ENABLE_CACHE" == "1" ]
-  then
-  #cache
-    ldapadd -H ldapi:/// -Y EXTERNAL -f /opt/pcache.ldif
-    ldapadd -H ldapi:/// -Y EXTERNAL -f /opt/pcachedb.ldif
-  fi
+    if [ "$ENABLE_CACHE" == "1" ]
+    then
+    #cache
+      ldapadd -H ldapi:/// -Y EXTERNAL -f /opt/pcache.ldif
+      ldapadd -H ldapi:/// -Y EXTERNAL -f /opt/pcachedb.ldif
+    fi
+
+    if [ "$ENABLE_MONITOR" == "1" ]
+    then
+      cat /opt/monitoring.ldif | envsubst >/tmp/monitoring.ldif
+      ldapadd -H ldapi:/// -Y EXTERNAL -f /tmp/monitoring.ldif
+    fi
   fi
 
   sleep 1
@@ -157,10 +169,9 @@ then
   kill $PID
 fi
 
-/opt/reopenldap/sbin/slapd -h "ldap://$HOSTNAME ldaps://$HOSTNAME ldapi:///" -u ldap -g ldap -d $LDAP_LOG_LEVEL -F /opt/reopenldap/etc/slapd.d &
+/opt/reopenldap/sbin/slapd -h "ldap://$HOSTNAME ldaps://$HOSTNAME ldapi:///" -u ldap -g ldap -d "$LDAP_LOG_LEVEL" -F "$CONFIG_DIR" &
 PID=$!
 sleep 1
-
 
 #switch to mirror mode
 ldapadd -H ldapi:/// -Y EXTERNAL -f /opt/mirror.ldif
@@ -168,9 +179,8 @@ ldapadd -H ldapi:/// -Y EXTERNAL -f /opt/mirror.ldif
 if [ "$MODE" == "BOOTSTRAP" ] || [ "$MODE" == "REPLICA" ]
 then
 #Convert additional schemas
-  for EXTSCHEMA in `ls -1 /opt/schemas/*.schema`
+  for EXTSCHEMA in $(ls -1 /opt/schemas/*.schema)
   do
-#  ES=${EXTSCHEMA#*-}
     echo "include $EXTSCHEMA" >/tmp/schemas.conf
     echo "Converting schema $EXTSCHEMA"
     echo 'runuser -l ldap -c "/opt/reopenldap/sbin/slaptest -f /tmp/schemas.conf -F $CONFIG_DIR"'
@@ -194,15 +204,19 @@ do
 done
 
 ldapadd -H ldapi:/// -Y EXTERNAL -f /tmp/tls.ldif
-#rm /tmp/tls.ldif
 
-cat /opt/maxsize.ldif | envsubst >/tmp/maxsize.ldif
-ldapadd -H ldapi:/// -Y EXTERNAL -f /tmp/maxsize.ldif
+# Override maxsize for database
+#cat /opt/maxsize.ldif | envsubst >/tmp/maxsize.ldif
+#ldapadd -H ldapi:/// -Y EXTERNAL -f /tmp/maxsize.ldif
 
 IFS=","
 #Add additional schemas
 for SCHEMA in $SCHEMAS
 do
+  echo "=============================================================="
+  echo "Processing schema $SCHEMA"
+  echo "=============================================================="
+  
   if [ -f $CONFIG_BASEDIR/schema/$SCHEMA.ldif ]
   then
     ldapadd -c -H ldapi:// -Y EXTERNAL -f $CONFIG_BASEDIR/schema/$SCHEMA.ldif
@@ -210,13 +224,14 @@ do
   fi
 done
 
-export FPDOMAIN=herzen
-cat /opt/domain.ldif | envsubst >/tmp/domain.ldif
-ldapadd -H ldapi:/// -Y EXTERNAL -f /tmp/domain.ldif
+#Build directory stub
+export FPDOMAIN="$(echo $LDAP_SUFFIX | sed 's/dc=\([^,]*\).*/\1/')"
+cat /opt/domain.ldif | envsubst >/tmp/domain0.ldif
+ldapadd -H ldapi:/// -Y EXTERNAL -f /tmp/domain0.ldif
 
-export FROOTDN=admin
-cat /opt/admin.ldif | envsubst >/tmp/admin.ldif
-ldapadd -H ldapi:/// -Y EXTERNAL -f /tmp/admin.ldif
+export FROOTDN="$(echo $ROOT_DN | sed 's/cn=\([^,]*\).*/\1/')"
+cat /opt/admin.ldif | envsubst >/tmp/admin0.ldif
+ldapadd -H ldapi:/// -Y EXTERNAL -f /tmp/admin0.ldif
 
 cat /opt/services.ldif | envsubst >/tmp/services.ldif
 ldapadd -H ldapi:/// -Y EXTERNAL -f /tmp/services.ldif
@@ -226,21 +241,23 @@ ldapadd -H ldapi:/// -Y EXTERNAL -f /tmp/replicator.ldif
 
 if [ "$MODE" != "REPLICA" ]
 then
-echo "dn: olcDatabase={-1}frontend,cn=config" >/tmp/limit.ldif
-echo "changetype: modify" >>/tmp/limit.ldif
-echo "replace: olcSizeLimit" >>/tmp/limit.ldif
-echo "olcSizeLimit: -1" >>/tmp/limit.ldif
-ldapadd -H ldapi:// -Y EXTERNAL -f /tmp/limit.ldif
+#Override search size limit to unlimited by default
+  echo "dn: olcDatabase={-1}frontend,cn=config" >/tmp/limit.ldif
+  echo "changetype: modify" >>/tmp/limit.ldif
+  echo "replace: olcSizeLimit" >>/tmp/limit.ldif
+  echo "olcSizeLimit: -1" >>/tmp/limit.ldif
+  ldapadd -H ldapi:// -Y EXTERNAL -f /tmp/limit.ldif
 
-echo "dn: olcDatabase={1}mdb,cn=config" >/tmp/limit.ldif
-echo "changetype: modify" >>/tmp/limit.ldif
-echo "replace: olcSizeLimit" >>/tmp/limit.ldif
-echo "olcSizeLimit: -1" >>/tmp/limit.ldif
-ldapadd -H ldapi:// -Y EXTERNAL -f /tmp/limit.ldif
+  echo "dn: olcDatabase={1}mdb,cn=config" >/tmp/limit.ldif
+  echo "changetype: modify" >>/tmp/limit.ldif
+  echo "replace: olcSizeLimit" >>/tmp/limit.ldif
+  echo "olcSizeLimit: -1" >>/tmp/limit.ldif
+  ldapadd -H ldapi:// -Y EXTERNAL -f /tmp/limit.ldif
 fi
 
 if [ "$ENABLE_ACCESSLOG" == "1" ] && [ "$MODE" != "REPLICA" ]
 then
+# If define access log - redefine max size
   echo "dn: olcDatabase={2}mdb,cn=config" >/tmp/limital.ldif
   echo "changetype: modify" >>/tmp/limital.ldif
   echo "replace: olcDbMaxSize" >>/tmp/limital.ldif
@@ -248,7 +265,7 @@ then
   ldapadd -H ldapi:// -Y EXTERNAL -f /tmp/limital.ldif
 fi
 
-#Modify LDAPs
+#Modify ACL
 echo "dn: olcDatabase={1}mdb,cn=config" >/tmp/access.ldif
 echo "changetype: modify" >>/tmp/access.ldif
 echo "replace: olcAccess" >>/tmp/access.ldif
@@ -260,6 +277,11 @@ then
     echo "olcAccess: {0}$ACCESS" >>/tmp/access.ldif
   done
 fi
+if [ "$ENABLE_ACCESSLOG" == "1" ]
+then
+  cat /tmp/accesslog_acl.ldif >>/tmp/access.ldif
+fi
+
 echo "olcAccess: {0}to * by dn.exact=gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth manage by dn.exact=uid=replicator,$LDAP_SUFFIX manage by * break" >>/tmp/access.ldif
 ldapadd -H ldapi:// -Y EXTERNAL -f /tmp/access.ldif
 
@@ -277,22 +299,51 @@ IFS=$OLD_IFS
 
 if [ "$MODE" == "BOOTSTRAP" ]
 then
-  export FPDOMAIN=herzen
+  export FPDOMAIN="$(echo $LDAP_SUFFIX | sed 's/dc=\([^,]*\).*/\1/')"
   cat /opt/domain.ldif | envsubst >/tmp/domain.ldif
   ldapadd -H ldapi:/// -Y EXTERNAL -f /tmp/domain.ldif
 
-  export PASSWORD="`slappasswd -h {ssha} -s $ROOT_PASSWORD`"
-  export FROOTDN=admin
+  export PASSWORD="$ENC_ROOT_PASSWORD"
+  export FROOTDN="$(echo $ROOT_DN | sed 's/dc=\([^,]*\).*/\1/')"
   cat /opt/admin.ldif | envsubst >/tmp/admin.ldif
   ldapadd -H ldapi:/// -Y EXTERNAL -f /tmp/admin.ldif
+
+  cat /opt/services.ldif | envsubst >/tmp/services.ldif
+  ldapadd -H ldapi:/// -Y EXTERNAL -f /tmp/services.ldif
+
+  cat /opt/replicator.ldif | envsubst >/tmp/replicator.ldif
+  ldapadd -H ldapi:/// -Y EXTERNAL -f /tmp/replicator.ldif
 fi
+
 sleep 1
 
 kill $PID
+
 echo "Running"
+
+if [ ! -z "$INITFILE" ]
+then
+  cat $INITFILE | slapadd -v -F $CONFIG_DIR
+fi
+
 if [ "$REINDEX" == "1" ] && [ "$MODE" != "REPLICA" ]
 then
   echo "Reindexing"
   /opt/reopenldap/sbin/slapindex -v
 fi
-/opt/reopenldap/sbin/slapd -h "ldap://$HOSTNAME ldaps://$HOSTNAME ldapi:///" -u ldap -g ldap -d $LDAP_LOG_LEVEL -F /opt/reopenldap/etc/slapd.d
+
+#Autorestart in case of error or failure
+while [ 1 ]
+do
+  declare -i BEFORE=`date +%s`
+  /opt/reopenldap/sbin/slapd -h "ldap://$HOSTNAME ldaps://$HOSTNAME ldapi:///" -u ldap -g ldap -D -d "$LDAP_LOG_LEVEL" -F "$CONFIG_DIR"
+  declare -i AFTER=`date +%s`
+  declare -i INTERVAL
+  INTERVAL=AFTER-BEFORE
+  echo "Duration $INTERVAL"
+  if [ $INTERVAL -lt 60 ]
+  then
+# Too fast restarts, quit
+    exit 1
+  fi
+done
